2d6b784676840a5f92dd7bd92726da1a8f06be27
Cátia Andreia Câmara Vieira <131257646+AndreiaCV@users.noreply.github.com>
2023-12-08 19:51:33 +0000

    Update invert.py

--- Blaze The Game/invert.py
+++ Blaze The Game/invert.py
@@ -7,10 +7,18 @@ SCREENHEIGHT = 600
 #invert powerup 
 class InvertPowerUp(PowerUp):
     def __init__(self, speed):
+        """
+        Initialize the Invert PowerUp.
+
+        Parameters:
+        - speed: Movement speed of the powerup on the screen.
+        """
         super().__init__('images/invert.png', effect_duration=10000, speed=speed, powerup_type='invert')
         self.elapsed_time = 0
-    #affect the player automatically when catching the powerup during a set amount of time
     def affect_player(self):
+         """
+        Affect the player when catching the powerup during a set amount of time.
+        """
         if self.active and self.player:
             current_time = pygame.time.get_ticks()
             elapsed_time = current_time - self.activation_time
@@ -22,6 +30,11 @@ class InvertPowerUp(PowerUp):
                 self.active = False
                 if isinstance(self.player, Car):
                     self.player.revert_commands()
-    #does not affect traffic
     def affect_traffic(self, traffic):
+        """
+        Does not affect traffic.
+
+        Parameters:
+        - traffic: List of traffic objects.
+        """
         pass
