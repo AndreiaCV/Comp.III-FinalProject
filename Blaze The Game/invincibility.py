a94f1077031923e8dcafb7eea6500b4d032efa63
Cátia Andreia Câmara Vieira <131257646+AndreiaCV@users.noreply.github.com>
2023-12-08 19:52:59 +0000

    Update invincibility.py

--- Blaze The Game/invincibility.py
+++ Blaze The Game/invincibility.py
@@ -4,15 +4,23 @@ from car import Car
 import random
 
 SCREENHEIGHT = 600
-#inviciblility powerup
+#inviciblility powerup class, inherits from PowerUp
 class InvincibilityPowerUp(PowerUp):
     def __init__(self, speed):
+        """
+        Initialize the Invincibility PowerUp.
+
+        Parameters:
+        - speed: Movement speed of the powerup on the screen.
+        """
         super().__init__('images/lightning.png', effect_duration=10000, speed=speed, powerup_type='invincibility')
         #starts not affecting any player
         self.player = None
         self.elapsed_time = 0
-   #Affects player for a set amount of time
     def affect_player(self):
+        """
+        Affect the player when catching the powerup during a set amount of time.
+        """
         if self.active and self.player:
             current_time = pygame.time.get_ticks()
             elapsed_time = current_time - self.activation_time
@@ -24,6 +32,11 @@ class InvincibilityPowerUp(PowerUp):
                 self.active = False
                 if isinstance(self.player, Car):
                     self.player.deactivate_invincibility()
-    #doesn't affect traffic
     def affect_traffic(self, traffic):
+         """
+        Does not affect traffic.
+
+        Parameters:
+        - traffic: List of traffic objects.
+        """
         pass
