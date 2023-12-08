405c5319428779dc4a8811e28cf0929e9e445a26
Cátia Andreia Câmara Vieira <131257646+AndreiaCV@users.noreply.github.com>
2023-12-08 19:55:25 +0000

    Update shoot.py

--- Blaze The Game/shoot.py
+++ Blaze The Game/shoot.py
@@ -2,16 +2,24 @@ import pygame
 from powerUp import PowerUp
 from car import *
 
-#shoot powerup
+#shoot powerup class, inherits from PowerUp
 class ShootPowerUp(PowerUp):
     def __init__(self, speed):
+         """
+        Initialize the ShootPowerUp object.
+
+        Parameters:
+        - speed: Movement speed of the powerup on the screen.
+        """
         super().__init__('images/gun.png', effect_duration=20000, speed=speed, powerup_type='shoot')
         self.bullets = []
         self.bullet_speed = 7
         self.elapsed_time = 0
         
-    #affects player for a given amount of time
     def affect_player(self):
+        """
+        Affect the player when catching the powerup during a set amount of time.
+        """
         if self.active and self.player:  
             current_time = pygame.time.get_ticks()
             elapsed_time = current_time - self.activation_time
@@ -25,8 +33,14 @@ class ShootPowerUp(PowerUp):
                     self.player.disable_shooting()
                     # Clean up bullets when power-up deactivates
                     self.bullets = []
-    #affects traffic
     def affect_traffic(self, traffic, images):
+        """
+        Affect the traffic when the powerup is active.
+
+        Parameters:
+        - traffic: List of traffic objects.
+        - images: Images used for the traffic/bullets.
+        """
             for bullet in self.bullets:
                 bullet.move()
                 for car in traffic:
@@ -38,15 +52,36 @@ class ShootPowerUp(PowerUp):
             self.bullets = [bullet for bullet in self.bullets if bullet.rect.y > 0]
     #shooting function
     def shoot(self, x, y):
+         """
+        Shoot a bullet from the specified position.
+
+        Parameters:
+        - x: X-coordinate of the bullet's starting position.
+        - y: Y-coordinate of the bullet's starting position.
+        """
         bullet = Bullet(x, y, self.bullet_speed)
         self.bullets.append(bullet)
 
     def get_bullets(self):
+        """
+        Get the list of active bullets.
+
+        Returns:
+        List of Bullet objects.
+        """
         return self.bullets
 
-
+# Bullet class for the ShootPowerUp
 class Bullet(pygame.sprite.Sprite):
     def __init__(self, x, y, speed):
+        """
+        Initialize the Bullet object.
+
+        Parameters:
+        - x: X-coordinate of the bullet's starting position.
+        - y: Y-coordinate of the bullet's starting position.
+        - speed: Speed of the bullet.
+        """
         super().__init__()
         self.image = pygame.Surface((5, 10))
         self.image.fill((255, 0, 0))  # Red bullet
@@ -56,4 +91,7 @@ class Bullet(pygame.sprite.Sprite):
         self.speed = speed
 
     def move(self):
+        """
+        Move the bullet upwards based on its speed.
+        """
         self.rect.y -= self.speed
