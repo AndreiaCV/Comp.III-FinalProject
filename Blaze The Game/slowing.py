66b3bc321642cf48539b08ab3a1ef9d886619a1b
Cátia Andreia Câmara Vieira <131257646+AndreiaCV@users.noreply.github.com>
2023-12-08 19:56:58 +0000

    Update slowing.py

--- Blaze The Game/slowing.py
+++ Blaze The Game/slowing.py
@@ -4,16 +4,33 @@ from car import Car
 import random
 
 SCREENHEIGHT = 600
-#slow traffic powerup
+#slow traffic powerup, inherits from PowerUp
 class SlowingPowerUp(PowerUp):
     def __init__(self, speed):
+        """
+        Initialize the Slowing PowerUp .
+
+        Parameters:
+        - speed: Movement speed of the powerup on the screen.
+        """
         super().__init__('images/snail.png', effect_duration=5000, speed=speed, powerup_type='slow')
         self.elapsed_time = 0
-    #doesn't affect player
     def affect_player(self, player):
+        """
+        Placeholder method for the player effect (no effect on the player).
+
+        Parameters:
+        - player: The player object.
+        """
         pass 
-    #affects oncoming cars velocity for a set amount of tima
+    #affects oncoming cars velocity for a set amount of time
     def affect_traffic(self, traffic):
+        """
+        Affect the oncoming traffic when the powerup is active.
+
+        Parameters:
+        - traffic: List of traffic objects.
+        """
         if self.active:
             current_time = pygame.time.get_ticks()
             elapsed_time = current_time - self.activation_time
