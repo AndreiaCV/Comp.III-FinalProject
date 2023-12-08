9afc3d9802ff9f278ad433993aac33dad45fff1c
Cátia Andreia Câmara Vieira <131257646+AndreiaCV@users.noreply.github.com>
2023-12-08 15:38:38 +0000

    Update interface.py

--- Blaze The Game/interface.py
+++ Blaze The Game/interface.py
@@ -97,13 +97,10 @@ def credits_():
 def instructions_():
     res = (720, 720)
     screen = pygame.display.set_mode(res)
-    comicsansfont = pygame.font.SysFont('Comic Sans MS', 25)
-    text_lines = [
-        comicsansfont.render('rule1 blah blah blah', True, YELLOW),
-        comicsansfont.render('rule2', True, YELLOW),
-        comicsansfont.render('rule3', True, YELLOW)
-    ]
+    backgrounds = ['images/instruçoes0.png', 'images/instruçoes1.png', 'images/instruçoes2.png', 'images/instruçoes3.png']
+    current_background_index = 0
     buttons = [create_button('images/back.png', 'images/back_pressed.png', pygame.Rect(450, 600, 200, 60), interface)]
+
 
     while True:
         mouse = pygame.mouse.get_pos()
@@ -111,19 +108,32 @@ def instructions_():
             if ev.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if ev.type == pygame.MOUSEBUTTONDOWN:
+            elif ev.type == pygame.MOUSEBUTTONDOWN:
                 for button in buttons:
                     if button[2].collidepoint(mouse):
                         button[3]()
+            elif ev.type == pygame.KEYDOWN:
+                if ev.key == pygame.K_LEFT:
+                    current_background_index = (current_background_index - 1) % len(backgrounds)
+                elif ev.key == pygame.K_RIGHT:
+                    current_background_index = (current_background_index + 1) % len(backgrounds)
 
-        screen.fill((0, 0, 0))
-        for i, text in enumerate(text_lines):
-            screen.blit(text, (0, i * 25))
+        # Load and display the current background image
+        background_image = pygame.image.load(backgrounds[current_background_index])
+        original_width, original_height = background_image.get_size()
+        
+        # Scale the image to almost the size of the display window
+        scale_factor = min(res[0] / original_width, res[1] / original_height)
+        scaled_width, scaled_height = int(original_width * scale_factor), int(original_height * scale_factor)
+        scaled_image = pygame.transform.scale(background_image, (scaled_width, scaled_height))
+        
+        # Calculate the position to center the image on the screen
+        x_offset = (res[0] - scaled_width) // 2
+        y_offset = (res[1] - scaled_height) // 2
+
+        screen.blit(scaled_image, (x_offset, y_offset))
         for button in buttons:
             image = button[1] if button[2].collidepoint(mouse) else button[0]
             screen.blit(image, button[2].topleft)
 
         pygame.display.update()
-
-if __name__ == "__main__":
-    interface()
